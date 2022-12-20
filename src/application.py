57dfb569afd09a8d0a1ca27e6db92d5d45a9b033
Greg M <gm2962@columbia.edu>
2022-12-19 20:48:25 -0600

    Adjusting request calls to work with cloud

--- src/application.py
+++ src/application.py
@@ -83,8 +83,8 @@ def get_product_by_id(product_id):
 
 @application.route("/create_product", methods=["POST"])
 def create_product():
-    data = json.loads(request.data)
-    print(json.dumps(data))
+    #data = json.loads(request.data)
+    data = json.loads(list(request.form.to_dict())[0])
 
     product_id = data["product_id"]
     name = data["name"]
